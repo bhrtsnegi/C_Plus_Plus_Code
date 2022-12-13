800060cf7aac556be9b8cccf03619ee028f475c2
Bharat Negi <1bhrtsnegi@gmail.com>
2022-12-13 21:11:06 +0530

    Add files via upload
    Changes in CPP Alternate swapping array code
    
    Input: 1,2,3,4,5,6
    Output: 2,1,4,3,6,5

--- alternate_swapping_array.cpp
+++ alternate_swapping_array.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 using namespace std;
 
+void get_array(int arr1[], int num){
+    for(int i=0; i<num; i++){
+        cout<<"Element "<<i+1<<"= ";
+        cin>>arr1[i];
+    }
+}
+void swap_array(int arr1[], int num){
+    int next;
+    for(int i=0; i<num; i++){
+        if(i%2==1){
+            next= arr1[i];
+            arr1[i]=arr1[i-1];
+            arr1[i-1]=next;
+        }
+    }
+}
+
+void print_array(int arr1[], int num){
+    for(int i=0; i<num; i++){
+        cout<<"Element "<<i+1<<": "<<arr1[i]<<endl;
+    }
+}
 int main(){
     int number, next;
     std::cout<<"Enter number of elements in array: ";
     cin>>number;
     int arr[number];
+    cout<<"Array Before Swapping"<<endl;
+    get_array(arr, number);
+    print_array(arr, number);
 
-    // Initialization of elements in array
-    for(int i=0; i<number; i++){
-        cout<<"Element "<<i+1<<"= ";
-        cin>>arr[i];
-    }
-
-    // Initial Array
-    cout<<"Array elements are"<<endl;
-    for(int i=0; i<number; i++){
-        cout<<"Element "<<i+1<<": "<<arr[i]<<endl;
-    }
-    // Swapping alternate logic
-    for(int i=0; i<number; i++){
-        if(i%2==1){
-            next= arr[i];
-            arr[i]=arr[i-1];
-            arr[i-1]=next;
-        }
-    }
-
-    // Array element after alternate swapping
     cout<<"After Swapping array elements are"<<endl;
-    for(int i=0; i<number; i++){
-        cout<<"Element "<<i+1<<": "<<arr[i]<<endl;
-    }
+    swap_array(arr, number);
+    print_array(arr, number);
+    
     return 0;
 }
