2a74c3f6037054e206b0b9871292bb720926e7f6
Bharat Negi <1bhrtsnegi@gmail.com>
2023-03-21 09:17:49 +0530

    Update autoKeyword.cpp

--- TypeInference/autoKeyword.cpp
+++ TypeInference/autoKeyword.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     auto x= 7.5;
-    auto y= &x; // ADDRESS OF x
+    auto y= &x; // ADDRESS OF x passed
 
     // typeid to tell the datatype for dynamic datatype 
     cout<< "X is having "<<typeid(x).name()<<"datatype"<<endl<<"X is having "<<typeid(y).name()<<"datatype";
